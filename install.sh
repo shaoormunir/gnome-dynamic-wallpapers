67282c69b17611346034aea6d89e90f9483cd626
Shaoor Munir <shaoormunir@outlook.com>
2020-05-17 01:21:46 +0500

    Added Firewatch wallpapers

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Lakeside
 cp -r Lakeside /usr/share/backgrounds
 chown -R root.root /usr/share/backgrounds/Lakeside
 chmod -R 755 /usr/share/backgrounds/Lakeside
@@ -7,3 +8,23 @@ chmod 644 /usr/share/backgrounds/Lakeside/Lakeside.xml
 
 cp desktop-backgrounds-lakeside.xml /usr/share/gnome-background-properties
 chmod 644 /usr/share/gnome-background-properties/desktop-backgrounds-lakeside.xml
+
+# Firewatch
+cp -r Firewatch /usr/share/backgrounds
+chown -R root.root /usr/share/backgrounds/Firewatch
+chmod -R 755 /usr/share/backgrounds/Firewatch
+chmod 644 /usr/share/backgrounds/Firewatch/Firewatch.xml
+
+cp desktop-backgrounds-firewatch.xml /usr/share/gnome-background-properties
+chmod 644 /usr/share/gnome-background-properties/desktop-backgrounds-firewatch.xml
+
+
+# Firewatch
+cp -r Firewatch-tower /usr/share/backgrounds
+chown -R root.root /usr/share/backgrounds/Firewatch-tower
+chmod -R 755 /usr/share/backgrounds/Firewatch-tower
+chmod 644 /usr/share/backgrounds/Firewatch-tower/Firewatch-tower.xml
+
+cp desktop-backgrounds-firewatch-tower.xml /usr/share/gnome-background-properties
+chmod 644 /usr/share/gnome-background-properties/desktop-backgrounds-firewatch-tower.xml
+
